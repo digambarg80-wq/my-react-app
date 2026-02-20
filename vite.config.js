374b24dde64df46e75b0de4b684d5c7bff9f5364
Digambar Raju Gaikwad <digambarg80@gmail.com>
2026-02-20 14:26:41 +0530

    trying update

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,12 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-// export default defineConfig({
-//   plugins: [
-//     react(),
-//     tailwindcss(),
-//   ],
-// })
-
-//vite.config.js
-
-//vite.config.js
+//src/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: "/",
   plugins: [
     react(),
     tailwindcss(),
   ],
-})
-
-
-
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_ID",
-  appId: "YOUR_APP_ID"
-};
-
-const app = initializeApp(firebaseConfig); //this connects our react app to our firebase project. without this -->nothing works.
-
-export const auth = getAuth(app);//This connects our app to firebase authentication --> this will handle: 1.Login, 2.Register, 3.Logout, 4.user session
-
-export const db = getFirestore(app);//This connects our app to Cloud Firestore this will store : 1. Products, 2. Users, 3. Orders, 4. Admin role
+});
